57daa6a997f1d99218ccc35ccea6c4eb7a923b5b
Bright_Effah <effahbright.eb@gmail.com>
2023-03-13 00:35:57 -0400

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
