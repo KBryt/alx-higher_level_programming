1a721d418c7b14909554c4a27ac51cfebda74167
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-03-14 00:37:37 -0400

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-import sys
-if __name__ == '__main__':
-    argc = len(sys.argv)
-    sum = 0
-    for i in range(1, argc):
-        num = int(sys.argv[i])
-        sum += num
-    print(sum)
+if __name__ == "__main__":
+    import sys
+    all = 0
+    for number in range(1, len(sys.argv)):
+        all += int(sys.argv[number])
+    print("{}".format(all))
