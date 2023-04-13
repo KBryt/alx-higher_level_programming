54ad2190dccc897bdb1f80ee11c5935a4c78fa04
KBryt <effahbright.eb@gmail.com>
2023-04-13 01:50:51 +0000

    update task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    """Print the a text file of a UTF8 to stdout."""
+    """Print the content of a UTF8 to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
