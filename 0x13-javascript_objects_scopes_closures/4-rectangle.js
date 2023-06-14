f668613d963cd36f4f5ca69b9e9d2468f34d0cc8
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-06-13 21:02:49 -0400

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,28 +1,23 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-  
-    rotate () {
-      let x = 0;
-      x = this.width;
-      this.width = this.height;
-      this.height = x;
-    }
-  
-    double () {
-      this.width *= 2;
-      this.height *= 2;
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log(('X'.repeat(this.width)));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+};
