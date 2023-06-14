1fc35f315d748377b11a31252246af782e7e9c8e
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-06-13 21:06:38 -0400

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let sum = 0;
-  for (const i of list) {
-    if (i === searchElement) {
-      sum += 1;
-    }
-  }
-  return sum;
+  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
